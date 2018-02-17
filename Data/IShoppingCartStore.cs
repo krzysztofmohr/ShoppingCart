80dff46dfd2410509a68f3554d9e72d91ad5f00b
krzysztof <krzysztof.mohr@live.com>
2018-02-17 16:47:25 +0000

    added shopping cart store service added shopping cart service added shopping cart item

---
+++ Data/IShoppingCartStore.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingCart.Data
+{
+  public interface IShoppingCartStore
+  {
+    ShoppingCart Get(int userId);
+    void Save(ShoppingCart shoppingCart);
+  }
+}
---
+++ Data/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace ShoppingCart.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ShoppingCart
+    {
+        private readonly HashSet<ShoppingCartItem> _items = new HashSet<ShoppingCartItem>();
+
+        public int UserId { get; }
+        public IEnumerable<ShoppingCartItem> Items => _items;
+
+        public ShoppingCart(int userId)
+        {
+            this.UserId = userId;
+        }
+
+        public void AddItems(IEnumerable<ShoppingCartItem> shoppingCartItems)
+        {
+            foreach (var item in shoppingCartItems)
+                this._items.Add(item);
+        }
+
+        public void RemoveItems(int[] productCatalogueIds)
+        {
+            _items.RemoveWhere(i => productCatalogueIds.Contains(i.ProductCatalogueId));
+        }
+    }
+}
