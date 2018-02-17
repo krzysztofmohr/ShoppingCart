950019775c6c262efd6df555eeddccd6cd1a3c68
krzysztof <krzysztof.mohr@live.com>
2018-02-17 16:50:40 +0000

    added dummy event store

---
+++ EventFeed/Event.cs
@@ -0,0 +1,6 @@
+﻿namespace ShoppingCart.EventFeed
+{
+    public class Event
+    {
+    }
+}
---
+++ EventFeed/EventStore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCart.EventFeed
+{
+    class EventStore : IEventStore
+    {
+        public IEnumerable<Event> GetEvents(long firstEventSequenceNumber, long lastEventSequenceNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Raise(string eventName, object content)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
